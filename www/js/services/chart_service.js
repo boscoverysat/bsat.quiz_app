4d3556f2bc3638186c8dee96bf2cafd2b057047a
Dailos Rafael Díaz Lara <ddialar@gmail.com>
2015-11-13 05:52:21 +0000

    [ CODE UPDATE ] - Edited introduction screen and scores one.

--- www/js/services/chart_service.js
+++ www/js/services/chart_service.js
@@ -10,22 +10,16 @@ app.service('chartService', function(){
 
     var data = [
       {
-        value: 300,
-        color:"#F7464A",
-        highlight: "#FF5A5E",
-        label: "Red"
+        value: 35,
+        color:"#0f3",
+        highlight: "#0f3",
+        label: "Aciertos"
       },
       {
-        value: 50,
-        color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Green"
-      },
-      {
-        value: 100,
-        color: "#FDB45C",
-        highlight: "#FFC870",
-        label: "Yellow"
+        value: 7,
+        color: "#ef473a",
+        highlight: "#ef473a",
+        label: "Errores"
       }
     ];
 
