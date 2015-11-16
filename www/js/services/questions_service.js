d0358d3b1d32515ae477e0b7c8c5bada26d1640b
ddialar <ddialar@gmail.com>
2015-11-16 07:15:59 +0000

    [ UPDATE ] - Questions screen edited.

---
+++ www/js/services/questions_service.js
@@ -0,0 +1,38 @@
+app.service('questionsService', function(){
+  this.questions = {
+
+  };
+});
+
+/*
+
+¿Es cierto que en el espacio no hay gravedad? No, es falso, en el espacio sí hay gravedad.  Sí, es cierto, en el espacio no hay gravedad. "¿Cuerpos flotando? No hay duda, en el espacio no hay gravedad ¿Cierto? ¡Error!
+
+La famosa Ley de la Gravitación Universal de sir Isaac Newton dice:
+
+""Dos masas cualesquiera (m1, m2) se atraen con una fuerza proporcional (F) al producto de sus masas e inversamente proporcional al cuadrado de las distancias (r) que las separan.""
+
+La fuerza gravitatoria se hace nula solo cuando la distancia entre dichas masas es infinita. No obstante el concepto de infinito es problemático cuando se traslada a la realidad, pues nunca seremos capaces de separar dos masas infinitamente, luego la fuerza gravitatoria nunca será 0. Se concluye así que el alcance de la gravedad es ilimitado, llega a todas partes; eso sí con una intensidad que varía según el cuadrado de la distancia. Como curiosidad: la gravedad a 400 Km de la superficie terrestre (donde se encuentra la ISS) es de tan solo un 10% menor que la que experimentamos habitualmente."  Medio
+¿satelite que orbita arededor de la tierra? La luna El sol  "La luna ya que es el unico satelite natural de la tierea , y gira alrededor de su eje (rotación) en aproximadamente 27.32dias .
+" Fácil
+
+¿Qué país lanzó el Sputnik? Unión Soviética Estados Unidos  La nave Sputnik 1 fue el primer intento no fallido de poner en órbita un satélite artificial alrededor de la Tierra. Se lanzó desde el Cosmódromo de Baikonur en Tyuratam, en Kazajistán, antes parte de la Unión Soviética Fácil
+¿Cómo se llamaba el primer animal que fue al espacio? Laika.  Tota. Laika (en ruso Лайка, ‘ladradora’; Moscú, 1954 - Sputnik 2, Órbita baja terrestre, 3 de noviembre de 1957) fue una perra espacial soviética que se convirtió en el primer ser vivo terrestre en orbitar la Tierra. Lo hizo a bordo de la nave soviética Sputnik 2, el 3 de noviembre de 1957. Fuente Wikipedia. Fácil
+¿Qué es el sol? Una estrella. Un planeta. Es una estrella, es una enorme esfera de gas caliente que brilla y gira.  Medio
+¿Cuántas veces es más grande el sol que la tierra?  1.300.000 veces 1000 veces. El sol es una estrella 1.300.000 veces mayor que la tierra. Aunque menos densa que la tierra. Medio
+¿Cómo brilla el sol?  Quemando hidrógeno. Aún no se sabe. El Sol brilla quemando hidrógeno y convirtiéndolo en helio en su centro Medio
+¿Por cuánto tiempo ha estado brillando el Sol?  4,6 billones de años. 1.000 millones de años. Se estima que el sol ha estado brillando 4,6 billones de años, y estará otros 5 billones de años más. Medio
+¿Cuál es la temperatura del sol?  5,600 grados Centigrados. No se sabe. La temperatura en la superficie es alrededor de 5.600 grados Centigrados. Medio
+¿Qué distancia hay de la tierra al sol? 150 millones de kilometros  36.252 kilometros.  Está a 150 millones de km. La luz del sol tarda unos 8 minutos en llegar a la tierra. Medio
+¿El sol gira sobre si mismo?  Si, gira o rota.  No, es falso no gira. El Sol a pesar de ser una esfera de gas gira sobre si mismo.  Medio
+¿El sol, al ser una esfera de gas, gira más rápido en el ecuador que en los polos?  Si, gira o rota más rápido en el ecuador. No, es falso no gira. El Sol al no ser un cuerpo sólido no gira uniformente, gira más rápido en el ecuador. Medio
+¿Las manchas solares son siempre las mismas?  No, cambian con el tiempo.  Si, siempre son iguales.  Algunas duran unas pocas horas,y otras duran semanas. Medio
+¿Mercurio es el planeta más cercano al sol? Si, es el más cercano.  No, es Venus. Mercurio es el más cercano, la distancia más cerca a la que llega a estar es de 46 millones de kilómetros.  Medio
+¿Cuánto dura un día en Mercurio?  58 días terrestres. 22 días terrestres. Mercurio gira muy lentamente y necesita 58 días terrestres para dar un giro sobre si mismo. Medio
+¿La fecha que aterrizó el primer objeto de origen humano sobre la superficie de Marte?  Fue el 27 de noviembre de 1971. Fue el 27 de noviembre de 1979. La sonda Mars 2 (también llamada Marsnik 2 o Marte 2) fabricada por la URSS, tomó tierra el 27 de noviembre de 1971.  Medio
+¿Cuál fue el primer satelite artificial de la historia? El Sputnik, de la URSS. El Apolo, de EEUU.  El Sputnik 1 lanzado el 4 de octubre de 1957 por la Unión Soviética fue el primer satélite artificial de la historia. Medio
+¿Cuál fue el primer satelite artificial Americano?  El Explorer,  El Challenger.  Fue lanzado a las 22:48 horas del 31 de enero de 1958.  Medio
+
+*/
+
+});
